06d677a0f8704f418973f3adf99960f03c28c118
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2021-12-15 17:17:30 +0100

    Slider prefab i arreglar cosetes

--- Scripts/UI_Slider.cs
+++ Scripts/UI_Slider.cs
@@ -5,14 +5,36 @@ using UnityEngine.UI;
 
 public class UI_Slider : MonoBehaviour
 {
-    Slider slider;
+    [SerializeField] Slider slider;
+    [SerializeField] Toggle mute;
     [SerializeField] float quantitat;
+    float rememberedValue = 1;
 
     private void OnEnable()
     {
-        if (slider == null) slider = GetComponent<Slider>();
+        if (slider.value == 0)
+        {
+            if (mute != null)
+            {
+                rememberedValue = 1;
+                mute.isOn = false;
+            }
+        }
     }
 
     public void Augmentar() => slider.value += quantitat;
     public void Disminuir() => slider.value -= quantitat;
+
+    public void Mute(bool mute)
+    {
+        if (mute)
+        {
+            rememberedValue = slider.value;
+            slider.value = 0;
+        }
+        else
+        {
+            slider.value = rememberedValue;
+        }
+    }
 }
