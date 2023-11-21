37ba96dbcbd83c67971ccefe74e7a443252ea300
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2023-11-21 23:02:55 +0100

    completar el que necessito

--- Scripts/UI_Toggle.cs
+++ Scripts/UI_Toggle.cs
@@ -6,9 +6,17 @@ using UnityEngine.UI;
 public class UI_Toggle : MonoBehaviour
 {
     [SerializeField] Slider slider;
+    [SerializeField] XS_Button button;
+
+
+    void OnEnable()
+    {
+        button.onClick.AddListener(Canviar);
+    }
 
     public void Canviar()
     {
         slider.value = slider.value == 0 ? 1 : 0;
+
     }
 }
